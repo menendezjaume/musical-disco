4592c2df4c674ff72096aaa3490288a0f4813fbd
Miguel A Menendez <m.menendezjaume@gmail.com>
2024-04-30 15:17:51 +0200

    Fix menu (#26)

--- main.py
+++ main.py
@@ -6,150 +6,167 @@ import os
 
 pygame.init()
 
-#icono
+# icono
 icono = pygame.image.load("logo.png")
 pygame.display.set_icon(icono)
 
-#Pantalla
+# Pantalla
 tamanyo = (1920, 1080)
 pantalla = pygame.display.set_mode(tamanyo, pygame.RESIZABLE)
 
-#Funciones para acceder a los juegos
+# Funciones para acceder a los juegos
+
+
 def astroRescue():
     wd = os.getcwd()
-    os.chdir("./1_AstroRescue")
-    subprocess.Popen("ls")    
+    os.chdir("./01_AstroRescue")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def marioBros():
     wd = os.getcwd()
-    os.chdir("./2_MarioBros")
-    subprocess.Popen("ls")    
+    os.chdir("./02_MarioBros")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def riskyRescue():
     wd = os.getcwd()
-    os.chdir("./3_RiskyRescue")
-    subprocess.Popen("ls")    
+    os.chdir("./03_RiskyRescue")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def spaceInvaders():
     wd = os.getcwd()
-    os.chdir("./4_SpaceInvaders")
-    subprocess.Popen("ls")    
+    os.chdir("./04_SpaceInvaders")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def flappyBird():
     wd = os.getcwd()
-    os.chdir("./5_FlappyBird")
-    subprocess.Popen("ls")    
+    os.chdir("./05_FlappyBird")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def rescateEspacial():
     wd = os.getcwd()
-    os.chdir("./6_RescateEspacial")
-    subprocess.Popen("ls")    
+    os.chdir("./06_RescateEspacial")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def dragonBall():
     wd = os.getcwd()
-    os.chdir("./7_DragonBall")
-    subprocess.Popen("ls")    
+    os.chdir("./07_DragonBall")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
 
+
 def planetDefenders():
     wd = os.getcwd()
-    os.chdir("./8_PlanetDefenders")
-    subprocess.Popen("ls")    
+    os.chdir("./08_PlanetDefenders")
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
 
 # def 9():
 #     wd = os.getcwd()
-#     os.chdir("./9_")
-#     subprocess.Popen("ls")    
+#     os.chdir("./09_")
+#     subprocess.Popen("ls")
 #     subprocess.Popen(["python3", "juego.py"])
 #     os.chdir(wd)
 # def 10():
 #     wd = os.getcwd()
 #     os.chdir("./10_")
-#     subprocess.Popen("ls")    
+#     subprocess.Popen("ls")
 #     subprocess.Popen(["python3", "juego.py"])
 #     os.chdir(wd)
+
 
 def spaceShooter():
     wd = os.getcwd()
     os.chdir("./11_SpaceShooter")
-    subprocess.Popen("ls")    
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def catInvaders():
     wd = os.getcwd()
     os.chdir("./12_CatInvaders")
-    subprocess.Popen("ls")    
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def asteroids():
     wd = os.getcwd()
     os.chdir("./13_Asteroids")
-    subprocess.Popen("ls")    
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def astroRain():
     wd = os.getcwd()
     os.chdir("./14_AstroRain")
-    subprocess.Popen("ls")    
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
+
 
 def skeletonAlienArmadaVsMexicanCat():
     wd = os.getcwd()
     os.chdir("./15_SkeletonAlienArmadaVsMexicanCat")
-    subprocess.Popen("ls")    
+    subprocess.Popen("ls")
     subprocess.Popen(["python3", "juego.py"])
     os.chdir(wd)
 
-# def 16():
-#     wd = os.getcwd()
-#     os.chdir("./16")
-#     subprocess.Popen("ls")    
-#     subprocess.Popen(["python3", "juego.py"])
-#     os.chdir(wd)
 
-# def 17():
-#     wd = os.getcwd()
-#     os.chdir("./17")
-#     subprocess.Popen("ls")    
-#     subprocess.Popen(["python3", "juego.py"])
-#     os.chdir(wd)
+def PlanetDefender():
+    wd = os.getcwd()
+    os.chdir("./16_PlanetDefender")
+    subprocess.Popen("ls")
+    subprocess.Popen(["python3", "juego.py"])
+    os.chdir(wd)
 
 
-pantalla.fill((255,255,255))
+def RetroPlane():
+    wd = os.getcwd()
+    os.chdir("./17_RetroPlane")
+    subprocess.Popen("ls")
+    subprocess.Popen(["python3", "juego.py"])
+    os.chdir(wd)
+
+
+pantalla.fill((255, 255, 255))
 
 pygame.display.flip()
 
 myimage = pygame_menu.baseimage.BaseImage(
-    image_path = "logo.png",
+    image_path="logo.png",
     drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL,
 )
 
 mytheme = pygame_menu.Theme(background_color=(myimage),
-                title_background_color=(0, 0, 0),
-                )
+                            title_background_color=(0, 0, 0),
+                            )
 
 
 pygame.display.flip()
-menu = pygame_menu.Menu('', pantalla.get_width(), pantalla.get_height(), theme=mytheme)
+menu = pygame_menu.Menu('', pantalla.get_width(),
+                        pantalla.get_height(), theme=mytheme)
 menu.add.button('Astro Rescue', astroRescue)
 menu.add.button('Mario Bros', marioBros)
 menu.add.button('Risky Rescue', riskyRescue)
@@ -158,15 +175,15 @@ menu.add.button('Flappy Bird', flappyBird)
 menu.add.button('Rescate Espacial', rescateEspacial)
 menu.add.button('Dragon Ball', dragonBall)
 menu.add.button('Planet Defenders', planetDefenders)
-menu.add.button('9', )
-menu.add.button('10', )
+# menu.add.button('9', )
+# menu.add.button('10', )
 menu.add.button('Space Shooter', spaceShooter)
 menu.add.button('Cat Invaders', catInvaders)
 menu.add.button('Asteroids', asteroids)
 menu.add.button('Astro Rain', astroRain)
 menu.add.button('Skeleton Alien Armada VS Mexican Cat', planetDefenders)
-menu.add.button('16', )
-menu.add.button('17', )
+menu.add.button('Planet Defender', PlanetDefender)
+menu.add.button('RetroPlane', RetroPlane)
 
 
 menu.mainloop(pantalla)
